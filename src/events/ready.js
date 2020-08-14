600c18259de2f1b1b5562c11748ecfe9de89a905
Wumpus <67572166+Pikachunou@users.noreply.github.com>
2020-08-14 13:58:20 -0700

    Update ready.js

--- src/events/ready.js
+++ src/events/ready.js
@@ -14,5 +14,16 @@ module.exports = class extends Event {
             `Loaded ${this.client.commands.size} commands!`,
             `Loaded ${this.client.events.size} events!`
         ].join('\n'));
+
+        const activities = [
+            `${this.client.guilds.cache.size} servers!`,
+            `${this.client.channels.cache.size} channels!`,
+            `${this.client.guilds.cache.reduce((a, b) => a + b.memberCount, 0)} users!`
+        ]
+
+        let i = 0;
+        setInterval(() => this.client.user.setActivity(`${this.client.prefix}help, lets pawn those noobs | Watching ${activities[i++ % activities.length]}`, { type: 'WATCHING' }), 20000);
+        //this.client.user.setPresence({ activity: {name: 'Pawning some noobs; use !help to help me!' }, status: 'dnd'});
     }
+
 };
