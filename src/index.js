590ede0229f92a38eb1a08da01293719678b5d7a
Wumpus <67572166+Pikachunou@users.noreply.github.com>
2020-08-27 22:44:56 -0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-const MenuDocsClient = require('./Structures/menudocsclient');
+const MenuDocsClient = require('./Structures/menudocsclient.js');
 const config = require('../config.json');
 
 const client = new MenuDocsClient(config);
